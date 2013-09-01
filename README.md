c1635a0f5c85246cca76992ba3a83ba53b91c750
Share <shaana@student.ethz.ch>
2013-09-01 14:20:11 +0200

    more useful version of install.sh

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-1) run ./install.sh to download the svn modules.
+1) run ./install.sh [-d [-c]] to download the svn modules. 
+(-c clean install, delete all existing folders; -d delete .svn subfolders)
 
 2) copy & paste these folders into your wow/AddOns folder
 AceGUI-3.0-SharedMediaWidgets/
--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+#delete folders (force option)
+if [ "$1" == "-c" ] || [ "$2" == "-c" ];
+	then
+	rm -rf SharedMedia
+	rm -rf LibSharedMedia-3.0
+	rm -rf AceGUI-3.0-SharedMediaWidgets
+	rm -rf SharedMediaAdditionalFonts
+fi
+
+#run svn
 /usr/bin/svn co svn://svn.wowace.com/wow/sharedmedia/mainline/trunk SharedMedia
 /usr/bin/svn co svn://svn.wowace.com/wow/libsharedmedia-3-0/mainline/trunk LibSharedMedia-3.0
 /usr/bin/svn co svn://svn.wowace.com/wow/ace-gui-3-0-shared-media-widgets/mainline/trunk AceGUI-3.0-SharedMediaWidgets
 /usr/bin/svn co svn://svn.wowace.com/wow/shared-media-additional-fonts/mainline/trunk SharedMediaAdditionalFonts 
 
 
-#uncomment this if you wann delte the .svn folders (no update possible)
-#rm -rf SharedMedia/.svn
-#rm -rf LibSharedMedia-3.0/.svn
-#rm -rf AceGUI-3.0-SharedMediaWidgets/.svn
-#rm -rf SharedMediaAdditionalFonts/.svn
+#uncomment this if you want delete the .svn folders (updating will no longer be possible)
+if [ "$1" == "-d" ] || [ "$2" == "-d" ];
+	then
+	rm -rf SharedMedia/.svn
+	rm -rf LibSharedMedia-3.0/.svn
+	rm -rf AceGUI-3.0-SharedMediaWidgets/.svn
+	rm -rf SharedMediaAdditionalFonts/.svn
+fi
